8ff1b6bf224381019ddbd7ea40718657c6fa7a60
Azat Bay QA <azatbay@gmail.com>
2023-03-03 23:41:11 +0300

    lesson & hw 03

--- LessonsHW/src/HW02.java
+++ LessonsHW/src/HW02.java
@@ -4,34 +4,38 @@ public class HW02 {
 //  Необходимо создать целочисленные переменные a и b,
 //  присвоить произвольные значения переменным на ваш выбор и вывести результаты
 //  следующих операций с этими переменными: сложение, умножение, вычитание, деление.
-        int a = 123;
-        int b = 100;
+        int a = 17;
+        int b = 4;
 
         System.out.println(a + b);
         System.out.println(a * b);
         System.out.println(a - b);
         System.out.println(a / b);
+        // Способы изменения типа на double
+        System.out.println(a * 1.0 / b); // * 1.0 используем для изменения типа на double
+        System.out.println((double)a / b); // Такой вариант не рекоммендуется
 
 //  Экстра задача
 //  Также вывести остаток от деления и сделать проверку на четность этих переменных.
-        double c = 6;
+        double c = 7;
         double d = 2;
         double ostatok = c % d;
 
 //Вывод остатка
-        System.out.println("Остаток деления = " + ostatok / d);
+        System.out.println("с mod d = " + c % d);
 
 // Проверка четности переменной c
-        if (c % 2 == 0)
-            System.out.println("Переменная с - Четная");
-        else
-            System.out.println("Переменная с - НЕчетная");
+        if (c % 2 == 0) {
+            System.out.println("Переменная с - Четное");
+        } else {
+            System.out.println("Переменная с - НЕ четное");
+        }
 // Проверка четности переменной d
-        if (d % 2 == 0)
-            System.out.println("Переменная d - Четная");
-        else
-            System.out.println("Переменная d - НЕчетная");
-
+        if (d % 2 == 0) {
+            System.out.println("Переменная d - Четное");
+        } else {
+            System.out.println("Переменная d - НЕ четное");
+        }
 //  Экстра задача
 //  Вывести/напечатать смайлик (не :), а настоящий смайлик одним символом).
         System.out.println("\uD83D\uDE00");
