d48008dc00f5abf358d280dab5dfdbf8e15024db
Azat Bay QA <azatbay@gmail.com>
2023-03-15 01:30:43 +0300

    lesson04 & HW04

--- LessonsHW/src/HW04.java
+++ LessonsHW/src/HW04.java
@@ -3,15 +3,27 @@ public class HW04 {
 
         /*Задача №1
 Необходимо вывести числа от 0 до 15.
-*/for (int i = 1; i <= 15; i = i + 1){
-            System.out.println(i);
+*/
+        System.out.println("Задача №1");
+
+        {
+            for (int i = 0; i <= 15; i = i + 1) {
+                System.out.println(i);
+            }
         }
 
 /*Задача №2
 Необходимо вывести все положительные степени числа
 5 которые меньше 10000, вывести результат возведения в степень.
  */
+        System.out.println();
+        System.out.println("Задача №2");
 
+        {
+            for (int i = 1; i < 10000; i = i * 5) {
+                System.out.println(i);
+            }
+        }
 
 /*Задача №3
 Необходимо вывести все числа кратные 4 между числами 40 и 60 включительно.
@@ -20,7 +32,24 @@ public class HW04 {
 2) без использования конструкции if (шаг цикла на ваше усмотрение).
 */
 
+//1) использовать конструкцию if для определения кратности (цикл с шагом 1, i = i + 1);
+        System.out.println();
+        System.out.println("Задача №3 1 способ");
+        {
+            for ( int i = 40; i <= 60; i = i + 1) {
+                if (i % 4 == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
 
+        System.out.println();
+        System.out.println("Задача №3 2 способ");
+        {
+            for (int i = 40; i <= 60; i+=4) {
+                System.out.println(i);
+            }
+        }
 
     }
 }
--- LessonsHW/src/lesson04.java
+++ LessonsHW/src/lesson04.java
@@ -1,5 +1,28 @@
 public class lesson04 {
     public static void main(String[] args) {
+        // в Java 4 цикла
+//        for (" до начала цикла "; "перед началом итерации" ; после каждой итерации )
+//            for ( объявление переменной; " возвращает true/false"; "двигает цикл вперед")
+
+        for (int i = 0; i < 5; i = i + 1){
+            System.out.println(i);
+            System.out.println("Hi!");
+        }
+
+        System.out.println("End");
+
+        // while do - цикл пердусловия
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            System.out.println("Hi!");
+            i = i + 1;
+        }
+
+        // do while - цикл с постусловием (перевернытый цикл while)
+//        do {
+//
+//        } while ();
 
     }
 }
